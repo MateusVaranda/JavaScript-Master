dae564ec7c1df90690ec9c14540b1b0cbb65f5fb
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-07-02 12:22:18 -0400

    return

---
+++ aula-03/return.js
@@ -0,0 +1,9 @@
+// return e quando retorna um valor para nos, ele devolve.
+
+function somar (a, b) {
+    return a + b;
+}
+
+const resultado = somar (10,50);
+
+console.log(resultado)
---
+++ aula-03/return02.js
@@ -0,0 +1,11 @@
+function somarValor (a, b) {
+    return a * b;
+
+}
+
+const x = somarValor(2, 3);
+const multiplicar = somarValor(x, 10);
+
+console.log(x)
+console.log(multiplicar)
+
