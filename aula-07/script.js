36c4de2d38d0f7a53a5f762945de1f18ad3798f8
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-07-04 20:39:55 -0400

    manipulando DOM

---
+++ aula-07/script.js
@@ -0,0 +1,17 @@
+const titulo = document.getElementById("titulo");
+
+titulo.textContent = "Bem-vindo Mateus";
+
+const texto = document.querySelector(".texto");
+
+texto.textContent = "Aprendendo DOM com JavaScript"
+
+const botao = document.getElementById("botao");
+
+botao.style.backgroundColor = "blue"
+botao.style.backgroundColor = "blue";
+botao.style.color = "white";
+botao.style.padding = "12px";
+botao.style.border = "none";
+botao.style.borderRadius = "8px";
+botao.style.cursor = "pointer";
